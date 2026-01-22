66b6fe20009cd79b5103b84850c9318436eaac9a
Ahmad Rifai <94911386+Rexus17@users.noreply.github.com>
2026-01-22 13:39:23 +0700

    add new fitur

--- app/(landing)/components/home/categories.tsx
+++ app/(landing)/components/home/categories.tsx
@@ -1,59 +1,53 @@
+import { getImageUrl } from "@/app/lib/api";
+import { Category } from "@/app/types";
 import Image from "next/image";
 import Link from "next/link";
-import {FiArrowRight} from "react-icons/fi";
+import { FiArrowRight } from "react-icons/fi";
 
 const categoryList = [
-    {
-        name: "Running",
-        imgUrl: "category-running.png",
-    },
-    {
-        name: "Tenis",
-        imgUrl: "category-tenis.png",
-    },
-    {
-        name: "Basketball",
-        imgUrl: "category-basketball.png",
-    },
-    {
-        name: "Football",
-        imgUrl: "category-football.png",
-    },
-    {
-        name: "Badminton",
-        imgUrl: "category-badminton.png",
-    },
-    {
-        name: "Swimming",
-        imgUrl: "category-swimming.png",
-    },
-]
+  {
+    name: "Running",
+    imgUrl: "category-running.png",
+  },
+];
 
-const CategoriesSection = () => {
+type TCategoriesProps = {
+  categories: Category[];
+};
 
-    return (
-        <section id="category-section" className="container mx-auto mt-50 pb-20">
-            <div className="flex justify-between">
-                <h2 className="font-bold text-2xl">Browse By Categories</h2>
-                <Link href="#" className="flex gap-2 text-primary font-medium">
-                    <span className="self-center">See All Categories</span>
-                    <FiArrowRight className="self-center" />
-                </Link>
+const CategoriesSection = ({categories}: TCategoriesProps) => {
+  return (
+    <section id="category-section" className="container mx-auto pt-30 pb-20 mt-50">
+      <div className="flex justify-between">
+        <h2 className="font-bold text-2xl">Browse By Categories</h2>
+        <Link href="#" className="flex gap-2 text-primary font-medium">
+          <span className="self-center">See All Categories</span>
+          <FiArrowRight className="self-center" />
+        </Link>
+      </div>
+      <div className="grid grid-cols-6 gap-12 mt-8">
+        {categories.map((category) => (
+          <div
+            className="rounded-lg bg-gradient-to-r from-[#F1F1F1] to-[#F7F7F7] w-full aspect-square flex justify-center"
+            key={category._id}
+          >
+            <div className="self-center">
+              <Image
+                src={getImageUrl(category.imageUrl)}
+                width={86}
+                height="86"
+                alt={category.name}
+                className="mb-[10px]"
+              />
+              <div className="text-primary font-medium text-xl text-center">
+                {category.name}
+              </div>
             </div>
-            <div className="grid grid-cols-6 gap-12 mt-8">
-                {
-                    categoryList.map((category, index)=> (
-                        <div className="rounded-lg bg-gradient-to-r from-[#F1F1F1] to-[#F7F7F7] w-full aspect-square flex justify-center" key={index}>
-                            <div className="self-center">
-                                <Image src={`/images/categories/${category.imgUrl}`} width={86} height="86" alt={category.name} className="mb-[10px]"/>
-                                <div className="text-primary font-medium text-xl text-center">{category.name}</div>
-                            </div>
-                        </div>
-                ))}
-            </div>
-        </section>
-    )
-
-}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default CategoriesSection;
