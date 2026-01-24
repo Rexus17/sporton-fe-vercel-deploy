4c955c2cbb7809c504cde00a27c8450397e26471
Ahmad Rifai <94911386+Rexus17@users.noreply.github.com>
2026-01-25 05:26:51 +0700

    add admin page

---
+++ app/lib/api.ts
@@ -0,0 +1,34 @@
+export async function fetchAPI<T>(
+    endpoint: string,
+    options?: RequestInit
+): Promise<T> {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${endpoint}`, {
+        ...options,
+        cache: options?.cache || "no-store",
+    });
+
+    if (!res.ok) {
+        let errorMessage = `Failed to fetch data from ${endpoint}`;
+        try {
+            const errorData = await res.json();
+            errorMessage = errorData.message || errorData.error || errorMessage;
+        } catch(e) {
+            console.log(e);
+        }
+
+        throw new Error(errorMessage);
+    }
+
+    return res.json();
+}
+
+export function getImageUrl(path: string) {
+    if (!path) return path;
+    if (path.startsWith("http")) return path;
+    return `${process.env.NEXT_PUBLIC_API_ROOT}/${path}`;
+}
+
+
+// const fallbackApiUrl = `https://be-sporton.agunacourse.com/api`;
+// const apiUrl = process.env.NEXT_PUBLIC_API_URL || fallbackApiUrl;
+// const res = await fetch(`${apiUrl}${endpoint}`, { ...options, cache: options?.cache || "no-store", });
